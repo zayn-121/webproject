4d05b9dab83c0720d59eeb145052df21b98dab66
btwGaurav <gauravsingh.gs.gs123@gmail.com>
2024-11-13 13:36:06 +0530

    new file created to store javascript pratise data

---
+++ Jawa practise/jawa1.js
@@ -0,0 +1,42 @@
+let marks = [98 ,12 ,22,33,44] ;
+console.log(marks);
+
+let Heroes = ["ironman" ,"batman ", "superman", "thor" ,"hulk"]
+
+for ( let idx = 0; idx < Heroes.length; idx++ ){
+    console.log(Heroes[idx]);
+}
+
+let cities = ["Rewa" , "Satna", "Patna" ,"Mumbai","Indore"]
+
+for (let city of cities){
+    console.log(city.toUpperCase()
+    );
+    
+}
+
+let sum = 0;
+for (let val of marks ){
+    sum = sum + val
+     
+} 
+
+console.log(sum)
+
+for (let value of items{
+    offer = value/10;
+    val= val-offer;
+})
+
+
+let items = [250,645,300,900,50]
+let i = 0;
+for (let val of items){
+    console.log(
+        `value at index ${i} = ${val}`);
+        let offer = val/10 ;
+        items[i] = items[i]-offer ;
+        console.log(`value after offer = ${ items[i] }`);
+        
+        i++;
+    }
