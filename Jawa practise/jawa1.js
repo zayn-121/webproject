fa5f8ab59be5e315d77a35574082a21fdf5d6db5
btwGaurav <gauravsingh.gs.gs123@gmail.com>
2024-11-14 11:07:50 +0530

    Made a new gradient btn with only one line of code

--- Jawa practise/jawa1.js
+++ Jawa practise/jawa1.js
@@ -1,42 +1,50 @@
-let marks = [98 ,12 ,22,33,44] ;
-console.log(marks);
+// let marks = [98 ,12 ,22,33,44] ;
+// console.log(marks);
 
-let Heroes = ["ironman" ,"batman ", "superman", "thor" ,"hulk"]
+// let Heroes = ["ironman" ,"batman ", "superman", "thor" ,"hulk"]
 
-for ( let idx = 0; idx < Heroes.length; idx++ ){
-    console.log(Heroes[idx]);
-}
+// for ( let idx = 0; idx < Heroes.length; idx++ ){
+//     console.log(Heroes[idx]);
+// }
 
-let cities = ["Rewa" , "Satna", "Patna" ,"Mumbai","Indore"]
+// let cities = ["Rewa" , "Satna", "Patna" ,"Mumbai","Indore"]
 
-for (let city of cities){
-    console.log(city.toUpperCase()
-    );
+// for (let city of cities){
+//     console.log(city.toUpperCase()
+//     );
     
-}
+// }
 
-let sum = 0;
-for (let val of marks ){
-    sum = sum + val
+// let sum = 0;
+// for (let val of marks ){
+//     sum = sum + val
      
-} 
+// } 
 
-console.log(sum)
+// console.log(sum)
 
-for (let value of items{
-    offer = value/10;
-    val= val-offer;
-})
+// for (let value of items{
+//     offer = value/10;
+//     val= val-offer;
+// })
 
 
-let items = [250,645,300,900,50]
-let i = 0;
-for (let val of items){
-    console.log(
-        `value at index ${i} = ${val}`);
-        let offer = val/10 ;
-        items[i] = items[i]-offer ;
-        console.log(`value after offer = ${ items[i] }`);
+// let items = [250,645,300,900,50]
+// let i = 0;
+// for (let val of items){
+//     console.log(
+//         `value at index ${i} = ${val}`);
+//         let offer = val/10 ;
+//         items[i] = items[i]-offer ;
+//         console.log(`value after offer = ${ items[i] }`);
         
-        i++;
-    }
+//         i++;
+//     }
+
+    let companies = ["bloomberg" ,"Microsoft","Uber","Google ","IBM","Netflix"];
+
+    companies.shift();
+
+    companies.splice(2,0,"ola ")
+
+    companies.push("Amazon")
