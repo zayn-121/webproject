7cf65a2f2cca105fbbf3dd231e013dc09e6f4a01
btwGaurav <gauravsingh.gs.gs123@gmail.com>
2024-11-20 14:25:51 +0530

    pratice

--- Jawa practise/jawa3.js
+++ Jawa practise/jawa3.js
@@ -5,3 +5,24 @@ console.log( myName );
 
 var a;
 var b = 2;
+
+let myDog = {
+ name : "ramu",
+ age : 17,
+ work : "gamer" ,
+
+}
+
+myDog.name = "Happy camper"
+
+console.log(myDog)
+
+var myArry = []
+
+var i = 1 ;
+while( i <= 5  ){
+    myArry.push(i);
+    i++
+}
+
+console.log(myArry)
