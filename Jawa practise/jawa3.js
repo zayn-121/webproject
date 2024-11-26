61aeb33907c9a80fdbf5da4c1cd6718679cee906
btwGaurav <gauravsingh.gs.gs123@gmail.com>
2024-11-26 11:19:40 +0530

    pratice

--- Jawa practise/jawa3.js
+++ Jawa practise/jawa3.js
@@ -1,12 +1,47 @@
-//chapter 2 
-//problem 2
-// let age = 33  ;
-// if(age>10 && age<18){
-//   console.log("your age lies btw 10 and 20")
-// }else {
-//   console.log("your age does not lies btw 10 and 20")
+// let a = 23;
+// let i = 0;
+// while(i<a){
+//   console.log(i)
+//   i++;
 // }
 
-let age = 17
-let a = age>18? "you can drive" : "you cannot drive"
-console.log(a)
+// function average(x,y){
+//   console.log("done")
+//   return Math. round(x+y)/2
+// }
+// let g = 34;
+// let b = 23
+// console.log("average of g and b is", average(g,b))
+
+// const sum = (x)=>{
+//   return x*x
+// }
+//   console.log(sum(4))
+// let marks = {
+//   harry : 90,
+//   subham : 9,
+//   lovish : 56,
+//   monika : 4
+  
+// }
+// for(i=0 ; i<Object.keys(marks).length; i++){
+//   console.log("Marks of "+ Object.keys(marks)[i] + " are " + 
+//              marks[Object.keys(marks)[i]])
+// }
+// const  mean = (a ,b,c,d)=>{
+//   return (a+b+c+d)/4}
+// console.log(mean(4,5,6,7))
+let i = 0;
+const count = (i)=>{
+  while(i<10){
+    i++
+    console.log(i)
+  }
+  
+}
+count(i)
+let str = []
+
+//nikhil is a harry pramod
+let sentence = `boy1 is a friend of boy 2`
+  console.log(sentence)
