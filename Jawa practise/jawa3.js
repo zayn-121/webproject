edbf24bc44de407f90db096816b557475173e824
btwGaurav <gauravsingh.gs.gs123@gmail.com>
2024-12-02 11:10:06 +0530

    practise jawa

--- Jawa practise/jawa3.js
+++ Jawa practise/jawa3.js
@@ -109,3 +109,25 @@ else{
   alert("let me please write it on the page")
 }
                   
+let runAgain = true;
+
+const canDrive = (age) => {
+  return age >= 18 ? true : false
+}
+
+while (runAgain) {
+  let age = prompt("Enter your age")
+  age = Number.parseInt(age)
+  if (age < 0) {
+    console.error("Please enter a valid age");
+    break;
+  }
+
+  if (canDrive(age)) {
+    alert("Yes you can drive")
+  }
+  else {
+    alert("You cannot drive")
+  }
+  runAgain = confirm("Do you want to play again?")
+}
