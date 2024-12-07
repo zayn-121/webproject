b36e776f9bed00eb856df074dcd698d8d448b987
btwGaurav <gauravsingh.gs.gs123@gmail.com>
2024-12-07 18:11:49 +0530

    Made a fully working clock

---
+++ Bulb/Clock/clock.js
@@ -0,0 +1,12 @@
+let a = new Date()
+let h = a.getHours()
+let m = a.getMinutes()
+let s = a.getSeconds()
+let d = a.getDate()
+console.log(h ,m ,s ,d)
+let time = () =>{
+document.querySelector(".hours1").innerHTML = h;
+document.querySelector(".Minutes1").innerHTML = m;
+document.querySelector(".seconds1").innerHTML = s;
+document.querySelector(".dates1").innerHTML = d;
+
