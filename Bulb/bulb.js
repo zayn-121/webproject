4cb4584f4193693dd5784f10447799b24fb9378a
zayn-121 <zayedanwar121@gmail.com>
2024-12-08 19:24:01 +0530

    modified some css and js

--- Bulb/bulb.js
+++ Bulb/bulb.js
@@ -1,17 +1,18 @@
-let press = "true";
-const bulbElement = document.querySelector("#bulbb");
-const buttonElement = document.querySelector("#btn1");
+const bulbElement = document.querySelector("#bulb");
+const inputElement = document.querySelector(".input");
+const circleElement = document.querySelector(".circle");
 
 // Toggle function to switch between "on" and "off"
-function toggle() {
-  if (press === "true") {
-    bulbElement.style.background = "gold"; // Turn on
-    press = "false";
-  } else {
-    bulbElement.style.background = "rgb(155, 170, 184)"; // Turn off
-    press = "true";
-  }
-}
 
-// Add a single event listener to the button
-buttonElement.addEventListener("click", toggle);
+
+inputElement.addEventListener("click", () => {
+  if (inputElement.checked) {
+    bulbElement.classList.add("on");
+    circleElement.innerText = "On";
+    console.log("bulb is on");
+  }else{
+    bulbElement.classList.remove("on");
+    circleElement.innerText = "Off";
+    console.log("bulb is off");
+  }
+})
